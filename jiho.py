ca54a4a26e6a805706894f6056be8fa6923c7577
Chikara Saito <nagulog@gmail.com>
2018-01-04 11:25:14 +0900

    [修正] 時報音の追加と時刻を知らせるセリフを変更する
    現在時刻を知らせる前に時報音を鳴るようにする．
    時刻を知らせるセリフが長かったためシンプルに時刻だけを知らせるように変更する．

--- jiho.py
+++ jiho.py
@@ -1,11 +1,10 @@
 # coding=utf-8
-from __future__ import print_function
 from os.path import join
 from os import getcwd, environ
+from pit import Pit
+from time import sleep
 from watson_developer_cloud import TextToSpeechV1
 import datetime
-from time import sleep
-from pit import Pit
 import subprocess
 
 def get_time(tts_id, tts_pass):
@@ -15,13 +14,14 @@ def get_time(tts_id, tts_pass):
         x_watson_learning_opt_out=True)  # Optional flag
 
     now = datetime.datetime.now()
-    time = str(now.hour) + '時' + str(now.minute) + '分ごろをお知らせします。'
+    time = str(now.hour) + '時' + str(now.minute) + '分'
 
     with open(join(getcwd(), 'now.wav'), 'wb') as audio_file:
         audio_file.write(text_to_speech.synthesize(time, accept='audio/wav', voice="ja-JP_EmiVoice"))
 
-def play_audio():
-    subprocess.call("aplay now.wav", shell=True)
+def play_audio(filename):
+    command = 'aplay ' + filename
+    subprocess.call(command, shell=True)
 
 
 if __name__ == '__main__':
@@ -32,4 +32,5 @@ if __name__ == '__main__':
         'tts_pass': ''}})
     get_time(user_info['tts_id'], user_info['tts_pass'])
     sleep(2)
-    play_audio()
+    play_audio('jihou.wav')
+    play_audio('now.wav')
